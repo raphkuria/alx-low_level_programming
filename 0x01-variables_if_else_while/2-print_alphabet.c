b696344dfb6ce153e2eea05efd9d591af5a67515
raphkuria <raphkuria>
2022-07-03 01:18:53 -0700

    prints the alphabet in lowercase, followed by a new line

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,19 +1,18 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
 
 /**
  * main - Entry point
  *
- * Return: Always 0 (Success)
+ * Return: Always 0 
+ *
  */
 
 int main(void)
 {
-	char low;
+	char a;
 
-	for (low = 'a'; low <= 'z'; low++)
-		putchar(low);
+	for (a = 'a'; a <= 'z'; a++)
+		putchar(a);
 		putchar('\n');
 
 	return (0);
