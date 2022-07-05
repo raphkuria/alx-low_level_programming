24d228d2775cb671c7580fe7946a03e5cc478899
raphkuria <raphkuria>
2022-07-05 12:45:00 -0700

    Putchar

---
+++ 0-putchar.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+* main - Prints "_putchar" followed by a new line
+*
+* Return: Always 0.
+*/
+
+int main(void)
+
+{
+	_putchar('_putchar');
+	_putchar('\n');
+
+	return (0);
+}
